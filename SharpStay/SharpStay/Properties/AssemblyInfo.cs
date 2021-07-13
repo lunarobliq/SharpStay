b216fb108eae969510b9476f71a9c0365b6bd459
ceramicskate0 <64501203+ceramic-skate0@users.noreply.github.com>
2021-07-13 13:04:44 -0400

    Update AssemblyInfo.cs

--- SharpStay/SharpStay/Properties/AssemblyInfo.cs
+++ SharpStay/SharpStay/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpStay")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpStay")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
